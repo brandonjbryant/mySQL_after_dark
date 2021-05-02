6728f9351c0cb88e87fe62d0e2ec6479022fb64e
“Brandon <brandon.j.bryant1@gmail.com>
2021-05-02 11:41:00 -0500

    bonus exercises prompt

---
+++ more_exercises.sql
@@ -0,0 +1,40 @@
+# Create a file named more_exercises.sql to do your work in. Write the appropriate USE statements to switch databases as necessary.
+
+-- Employes Database
+
+# How much do the current managers of each department get paid, relative to the average salary for the department?
+
+# Is there any department where the department manager gets paid less than the average salary?
+
+-- World Database
+-- Use the `world` database for the questions below.
+
+# What languages are spoken in Santa Monica?
+
+# How many different countries are in each region?
+
+# What is the population for each region?
+
+# What is the population for each continent?
+
+# What is the average life expectancy globally?
+
+# What is the average life expectancy for each region, each continent? Sort the results from shortest to longest
+
+# Bonus
+
+# Find all the countries whose local name is different from the official name
+
+# How many countries have a life expectancy less than x?
+# What state is city x located in?
+
+# What region of the world is city x located in?
+
+# What country (use the human readable name) city x located in?
+
+# What is the life expectancy in city x?
+
+
+
+-- Sakila Database
+
